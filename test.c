9aafebedb153ab9940025eabf9b3dc303fbb84a1
username <julykota0713@gmail.com>
2019-06-10 17:11:27 +0900

    first commit

---
+++ test.c
@@ -0,0 +1,15 @@
+
+// input.cpp
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a, b;
+    cin >> a >> b;
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    
+    return 0;
+} 
